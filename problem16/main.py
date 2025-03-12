bac18aaf5a3486ab42805254c6c36cd13545d625
clegwhs <CQL718@gmail.com>
2025-03-12 12:08:00 -0400

    trying-to-solve

--- problem16/main.py
+++ problem16/main.py
@@ -1,10 +1,29 @@
 import sys
+from test import get_reciprocal_digits, find_cycle_start, CycleError
 
 def main():
     cases = int(sys.stdin.readline().rstrip())
     for caseNum in range(cases):
-        data = sys.stdin.readline().rstrip()
-
+        n = int(sys.stdin.readline().rstrip())
+        D = len(str(n))  # length of the initial division
+        Q = 10**D//n    # first digit of the division
+        R = 10**D % n   # remainder after division
+        # Creates a generator object.
+        digit_generator = get_reciprocal_digits(n, Q, R)
+        # Initializes the digits with the proper place value
+        digits = f'{"0"*(D-1)}'
+        # Generates the first 2*(n-1)+n digits of the number to ensure we have enough digits for finding a cycle
+        for _ in range(2*(n-1)+n):
+            try:
+                # Next method is called on the digit generator to generate the next digit
+                digits += str(next(digit_generator))
+            # Exception raised when there are no more divisions, so we break out of this loop.
+            except StopIteration:
+                break
+            except CycleError:
+                print("Cycle detected!")
+                break
+        print(n, find_cycle_start(digits), digits)
 
 if __name__ == "__main__":
     main()
--- problem16/test.py
+++ problem16/test.py
@@ -1,5 +1,3 @@
-from functools import cache
-
 # Long division with a reciprocal works as follows:
 # 7 | 1.000... -> We have a divisor (n=7), a dividend (d=1.000...), and it results in a quotient (Q) and a remainder (R).
 # We are interested in generating the digits of Q.
@@ -7,15 +5,15 @@ from functools import cache
 # Because of how the algorithm repeats, we can use recursion to yield digits.
 
 # Ex: 1/13
-# First division:   100/13 => 11Q + R: Q=7, R=9
-# Second division:  90/13  => 11Q + R: Q=6, R=12
-# Third division:  120/13  => 11Q + R: Q=9, R=3
-# Fourth division: 30/13   => 11Q + R: Q=2, R=4
-# Fifth division:  40/13   => 11Q + R: Q=3, R=1
+# First division:   100/13 => 13Q + R: Q=7, R=9
+# Second division:  90/13  => 13Q + R: Q=6, R=12
+# Third division:  120/13  => 13Q + R: Q=9, R=3
+# Fourth division: 30/13   => 13Q + R: Q=2, R=4
+# Fifth division:  40/13   => 13Q + R: Q=3, R=1
 # ...
-# Generally:      10*R/n   => 11Q + R: Q=(10*R)//n, R=10*R % n
+# Generally:      10*R/n   =>  nQ + R: Q=(10*R)//n, R2=10*R1 % n
 
-# Step 1a: Determine the first division. This requires that we know how many digits (D) are in n - we can use len(n) to get that.
+# Step 1a: Determine the first division. This requires that we know how many digits (D) are in n - we can use len(str(n)) to get that.
 #          Then, the first division is (10**D)/n. In the above case, we had D=2, so our first division was 100/13.
 # Step 1b: The first digit of this division is however many times n can fit into 10**D - mathematically expressed, we can figure that out by using the expression x = 10**D//n.
 #          The floor division allows us to discard the decimal portion of a division and only keep the integer part.
@@ -34,16 +32,25 @@ from functools import cache
 # Now, we can create a generator function. Generators can have the function next() applied to them to to calculate the next item in a sequence without having to precompute the digits beforehand.
 # This is useful in this case because we don't know how many digits a cycle will take, only that it cannot have a length greater than the number itself.
 
+class CycleError(Exception):
+    def __init__(self, message, details=None):
+        super().__init__(message)
+        self.details = details
+    def __str__(self):
+        return f"self.__class__.__name__: {self.args[0]} \nDetails: {self.details}"
 
 def get_reciprocal_digits(n, Q, R):
-    quotient = 10*R // n
-    remainder = 10*R % n
+    old_quotient = Q
+    old_remainder = R
+    new_quotient = 10*R // n
+    new_remainder = 10*R % n
     yield Q
     if R == 0:
         return StopIteration
+    elif old_quotient == new_quotient and old_remainder == new_remainder:
+        return CycleError
     else:
-        yield from get_reciprocal_digits(n, quotient, remainder)
-
+        yield from get_reciprocal_digits(n, new_quotient, new_remainder)
 
 # Cycle detection algorithm: Floyd's Tortoise and Hare
 # https://visualgo.net/en/cyclefinding
@@ -54,10 +61,10 @@ def get_reciprocal_digits(n, Q, R):
 # For example, consider the following graph for 1/14:
 #          1 -> 4
 #        /       \
-# 0 -> 7          2
+# 0 -> 7          2  : Our generator can keep generating the digits necessary here.
 #       \        /
 #        5  <- 8
-# The pointers would traverse the graph with the following numbers:
+# The pointers would traverse the graph (jump by index) with the following numbers. When a match is detected, the tortoise position resets back to 0, and the tortoise and the hare each traverse the graph one node at a time.
 # Tortoise: [0, 7, 1, 4, 2, 8, (5)] -> Cycle detected -> [0, (7)] -> Cycle starts at 7 (or index position 1)
 # Hare:     [0, 1, 2, 5, 1, 2, (5)] -> Cycle detected -> [5, (7)]
 # Another example: consider 1/17
@@ -68,20 +75,24 @@ def get_reciprocal_digits(n, Q, R):
 #   7 <- 4 <- 6 <- 7 <- 1 <- 1 <- 4
 # Tortoise: [0,5,8,8,2,3,5,2,9,4,1,1,7,6,4,7,(0)] -> Cycle detected -> [(0)] -> Cycle starts at 0 (or index position 0)
 # Hare:     [0,8,2,5,9,1,7,4,0,8,2,5,9,1,7,4,(0)] -> Cycle detected -> [(0)]
-for n in range(2, 20):
-    D = len(str(n))  # length of the initial division
-    Q = 10**D//n    # first digit of the division
-    R = 10**D % n   # remainder after division
-    # Creates a generator object.
-    digit_generator = get_reciprocal_digits(n, Q, R)
-    # Initializes the digits with the proper place value
-    digits = f'{"0"*(D-1)}'
-    # Generates the first 2*(n-1) digits of the number to ensure we have enough digits for finding a cycle
-    for _ in range(2*(n-1)):
+
+def find_cycle_start(array):
+    tortoise = 0
+    hare = 0
+    cycle_detected = False
+    cycle_length = None
+    while hare <= len(array):
         try:
-            # Next method is called on the digit generator to generate the next digit
-            digits += str(next(digit_generator))
-        # Exception raised when there are no more divisions, so we break out of this loop.
-        except StopIteration:
+            tortoise += 1
+            hare += 2
+            if array[tortoise] == array[hare]:
+                cycle_detected = True
+                cycle_length = tortoise
+                tortoise = 0
+                while array[tortoise] != array[hare]:
+                    tortoise += 1
+                    hare += 1
+                return (tortoise, array[tortoise], cycle_length)
+        except:
             break
-    print(n, digits)
+    return None
